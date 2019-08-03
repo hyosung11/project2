87595903122efea1403f367ddd706497de259dca
HyoSung Bidol-Lee <hyosung11@me.com>
2019-08-03 12:55:39 -0400

    added seed data

--- controllers/movements.js
+++ controllers/movements.js
@@ -4,12 +4,43 @@ const router = express.Router()
 const Movement = require('../models/movements.js')
 
 // ROUTES
+// Seed Route
+router.get('/seed', (req, res)=>{
+    Movement.create(
+      [
+        {
+            name: 'Pull',
+            image: 'pullup',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            example: 'pullup'
+        },
+        {
+            name: 'Push',
+            image: 'pushup',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            example: 'pushup'
+        },
+        {
+            name: 'Hinge',
+            image: 'deadlift',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            example: 'deadlift'
+        },
+        {
+            name: 'Squat',
+            image: 'goblet squat',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            example: 'goblet squat'
+        }
+    ], (error, data) => {
+        res.redirect('/movements');
+    })
+})
 
-// Seed
 
 // 1. Index Route
 
 // 2.
 
 
-module.exports = router 
+module.exports = router
--- models/movements.js
+++ models/movements.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const movementSchema = new mongoose.Schema ({
   name: { type: String, required: true },
   image: String,
+  description: String,
   example: String
 })
 
