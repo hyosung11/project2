5df93e9a9db9f6c5de93768de858a1779e5fd714
HyoSung Bidol-Lee <hyosung11@me.com>
2019-08-03 11:39:28 -0400

    server.js setup

--- server.js
+++ server.js
@@ -1,60 +1,54 @@
-// console.log(process.env.MONGODB_URI);
-//___________________
-//Dependencies
-// ___________________
-const express = require('express')
-const methodOverride  = require('method-override')
-const mongoose = require ('mongoose')
-const app = express ()
+// DEPENDENCIES
+const app = express()
 const db = mongoose.connection
-//___________________
-//Port
-//___________________
-// Allow use of Heroku's port or your own local port, depending on the environment
-const PORT = process.env.PORT || 3000;
+const express = require('express')
+const mongoose = require ('mongoose')
 
-//___________________
-//Database
-//___________________
-// How to connect to the database either via heroku or locally
-const MONGODB_URI = process.env.MONGODB_URI;
-
-// Fix Depreciation Warnings from Mongoose*
-// May or may not need these depending on your Mongoose version
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-
-// Connect to Mongo
-mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
-
-// Error / success
-db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
-db.on('disconnected', () => console.log('mongo disconnected'));
-
-//___________________
-//Middleware
-//___________________
-
+// MIDDLEWARE
+// include the method-override package
+const methodOverride  = require('method-override')
 //use public folder for static assets
-app.use(express.static('public'));
-
+app.use(express.static('public'))
 // populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
-app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
-
+app.use(express.urlencoded({ extended: false }))// extended: false - does not allow nested objects in query strings
+app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project.
 //use method override
 app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 
-//___________________
-// Routes
-//___________________
+// connect to the movements controller
+const movementsController = require('./controllers/movements.js')
+app.use('/movements', movementsController)
+
+// PORT
+// Allow use of Heroku's port or your own local port, depending on the environment
+const PORT = process.env.PORT || 3000
+
+// DATABASE
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI
+
+// Fix Depreciation Warnings from Mongoose*
+// May or may not need these depending on your Mongoose version
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
+
+// Connect to Mongo
+mongoose.connect(MONGODB_URI , { useNewUrlParser: true})
+mongoose.connection.once('open', () => {
+  console.log('connected to Pineapple')
+})
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'))
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI))
+db.on('disconnected', () => console.log('mongo disconnected'))
+
+
+// ROUTES => controllers 
 //localhost:3000
 app.get('/' , (req, res) => {
-  res.send('luminous minds working together');
+  res.send('luminous minds working together')
 });
 
-//___________________
-//Listener
-//___________________
-app.listen(PORT, () => console.log( 'Listening and ready on port:', PORT));
+// LISTENER
+app.listen(PORT, () => console.log( 'Listening and ready on port:', PORT))
