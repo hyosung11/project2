d99b54297bf8c705ea68c8cb76aea364f78ccba8
HyoSung Bidol-Lee <hyosung11@me.com>
2019-08-04 19:06:40 -0400

    show page start

--- controllers/movements.js
+++ controllers/movements.js
@@ -26,7 +26,7 @@ router.get('/seed', (req, res) => {
             name: 'Hinge',
             image: '/images/deadlift.png',
             info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            example: 'one-leg Romanian deadlift',
+            example: 'kettlebell deadlift',
             moreExamples: 'kettlebell swing, barbell hip thrust, Romanian single leg deadlift'
         },
         {
